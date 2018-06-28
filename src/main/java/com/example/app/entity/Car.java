3f53590ce5b0f0416b8fdee8da6a2d32a6a9c353
agatav <ahatavaliukevich@gmail.com>
2018-06-28 12:18:37 +0200

    Add Entity annotation. Rename CarStatus.

--- src/main/java/com/example/app/entity/Car.java
+++ src/main/java/com/example/app/entity/Car.java
@@ -1,42 +1,38 @@
 package com.example.app.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-
-//@Entity
+@Entity
+@Table(name = "car")
 public class Car {
-
+    @Id
+    @GeneratedValue
     private int id;
     private String carNumber;
-    private CarLocation carLocation;
-    private CarSatus carSatus;
 
+    @Column(name = "car-location")
+    private CarLocation carLocation;
+    @Column(name = "car-status")
+    private CarStatus carStatus;
+
+    //@OneToMany(mappedBy = "parkingMeter")
+    //private Set<ParkingMeter> meters = new HashSet<>();
 
     private ParkingMeterStatus parkingMeterStatus;
 
     private Car() { } // JPA only
 
 
-    public Car(int id, String carNumber, CarLocation carLocation, CarSatus carSatus, ParkingMeterStatus parkingMeterStatus) {
+    public Car(int id, String carNumber, CarLocation carLocation, CarStatus carStatus, ParkingMeterStatus parkingMeterStatus) {
         this.setId(id);
         this.setCarNumber(carNumber);
         this.setCarLocation(carLocation);
-        this.setCarSatus(carSatus);
+        this.setCarStatus(carStatus);
         this.setParkingMeterStatus(parkingMeterStatus);
 
     }
-//
-//    @OneToMany(mappedBy = "car")
-//    private Set<ParkingMeter> parkingMeterSet = new HashSet<>();
-//
-//    public Set<ParkingMeter> parkingMeterSet() {
-//        return parkingMeterSet;
-//    }
 
     public int getId() {
         return id;
@@ -63,12 +59,12 @@ public class Car {
         this.carLocation = carLocation;
     }
 
-    public CarSatus getCarSatus() {
-        return carSatus;
+    public CarStatus getCarStatus() {
+        return carStatus;
     }
 
-    public void setCarSatus(CarSatus carSatus) {
-        this.carSatus = carSatus;
+    public void setCarStatus(CarStatus carStatus) {
+        this.carStatus = carStatus;
     }
 
     public ParkingMeterStatus getParkingMeterStatus() {
@@ -79,13 +75,17 @@ public class Car {
         this.parkingMeterStatus = parkingMeterStatus;
     }
 
+//    public Set<ParkingMeter> getMeters() {
+//        return meters;
+//    }
+
     @Override
     public String toString() {
-        return "Blog{" +
+        return "Car{" +
                 "id=" + id +
                 ", car Number='" + carNumber + '\'' +
                 ", car location='" + carLocation + '\'' +
-                ", car status='" + carSatus + '\'' +
+                ", car status='" + carStatus + '\'' +
                 ", parking meter status='" + parkingMeterStatus + '\'' +
                 '}';
     }
